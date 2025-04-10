d358555b5d9852184bf66364cb41e90fb2aee8e4
steven-xuk <xuk@scotscollege.school.nz>
2025-04-11 09:13:01 +1200

    add Login and Signup components with basic structure

---
+++ src/components/Signup.js
@@ -0,0 +1,8 @@
+export default function Signup() {
+    return (
+        <div className="signup">
+            <h1>Signup Page</h1>
+            <p>Welcome to the signup page!</p>
+        </div>
+    );
+}
