bde3d986119d20677f237fda8d49e262132f2527
Ice_creme <141097782+ice-creme-studios@users.noreply.github.com>
2025-04-30 11:32:51 +1200

    user can upload image into the step

--- src/components/CreateWalk.jsx
+++ src/components/CreateWalk.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'; 
+import { supabase } from '../SupabaseClient';
+
 
 function CreateWalk() {
 
@@ -12,7 +15,18 @@ function CreateWalk() {
             comments: [],
         },
         userData: {},
+        walkID: ''
     })
+
+    const ID = uuidv4()
+
+    useEffect(() => {
+        setWalk(prev => ({
+            ...prev,
+            walkID: ID
+        }))
+    }, [])
+
 
     const [currentStep, setCurrentStep] = useState('making steps')
 
@@ -20,13 +34,37 @@ function CreateWalk() {
         title: '',
         description: '',
         tip: '',
-        img: '',
         pin: '',
-        helpTXT: ''
+        help: '',
+        img: '',
     })
+
+    const [stepImg, setStepIMG] = useState(null)
+
 
     function addStep(e){
         e.preventDefault()
+
+        const fileExt = stepImg.name.split('.').pop()
+        const fileName = `${Date.now()}.${fileExt}-${ID}`
+        const filePath = `walks/${ID}/${fileName}`;
+
+        supabase
+            .storage
+            .from('wayfare') // replace with your bucket name
+            .upload(filePath, stepImg)
+            .then(({ data, error }) => {
+                if (error) {
+                    console.error('Upload error:', error.message);
+                } else {
+                    console.log('Uploaded to:', data.path);
+                }
+            });
+
+
+        
+
+    
     }
 
     function publishWalk(){
@@ -40,6 +78,10 @@ function CreateWalk() {
         }));
     }
 
+    function handleImgUpload(e){
+        setStepIMG(e.target.files[0])
+    }
+
     return (
         <div>
             <p>create a walk!</p>
@@ -51,6 +93,11 @@ function CreateWalk() {
                         <input name='title' type="text" value={stepFormData.title} placeholder='add a title' required onChange={e => handleChange(e)}/>
                         <textarea rows="10" cols="50" name='description' type='tel' value={stepFormData.description} placeholder='add a description' required onChange={e => handleChange(e)}/>
                         <input name='tip' type="text" value={stepFormData.tip} placeholder='add a tip' required onChange={e => handleChange(e)}/>
+                        <input name='img' type="file" placeholder='add an image' required onChange={e => handleImgUpload(e)} accept="image/*"/>
+                        <input name='pin' type='text'  value={stepFormData.pin} placeholder='add a link to google maps' required onChange={e => handleChange(e)}/>
+                        <input name='help' type="text" value={stepFormData.help} placeholder="add a some 'help' text" onChange={e => handleChange(e)}/>
+                        <button type='submit'>create step</button>
+                        <button type='submit'>create step and finish</button>
                     </form>
                 
                 </div>}
