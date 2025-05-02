49ae9da5fcdec65350dd45f2ae2344c2cf1e8eb2
steven-xuk <xuk@scotscollege.school.nz>
2025-05-02 17:04:47 +1200

    made home auth and home navbar into a separate compoennt

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,90 +1,10 @@
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { supabase } from '../SupabaseClient.js';
-import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { NavLink } from "react-router-dom";
-import logo from '../imgs/logo.png';
-import profile from '../imgs/profile.png';
-import mountain from '../imgs/mountain.png';
+import { Link } from "react-router-dom";
+import HomeNavbarAuth from './parts/HomeNavbarAuth.js';
 
 export default function Home() {
-
-  const [userDataObj, setUserDataObj] = useState()
-  const [isAccountMenuOpen, setIsAccountMenuOpen] = useState(false);
-  const navigate = useNavigate();
-
-  const dispatch = useDispatch();
-  const authState = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (authState.isLoggedIn === false && authState.updated === true) {
-      navigate('/login')
-      console.log('User not logged in, redirecting to login page1231');
-    }
-  }, [authState])
-
-  async function handleLogout() {
-      const { error } = await supabase.auth.signOut();
-      if (error) {
-        console.error('Logout error:', error.message);
-      } else {
-        console.log('Logged out successfully!');
-        // No need to manually dispatch logout() because App.js handles it automatically
-      }
-  }
-
-  async function getTokenData() {
-    if (authState.isLoggedIn === false && authState.updated === true) {
-      navigate('/login')
-      console.log('User not logged in, redirecting to login page1231');
-      return
-    }
-
-    const { data, error } = await supabase.auth.getSession();
-    if (error) {
-      console.error('Error getting session:', error);
-      return null;
-    }
-
-    const { data: userData, error: userError } = await supabase
-      .from('Users')      
-      .select('*')
-      .eq('UID', data.session?.user.user_metadata.display_name)
-      .single();
-  
-    if (userError) {
-      console.error('Error fetching row:', userError);
-    }
-    if (userData){
-      // console.log('Fetched row:', userData);
-    }
-    
-    setUserDataObj({
-      ...userData,
-      date_joined: userData.created_at,
-      email: data.session?.user.email,
-      isAuthenticated: data.session?.user.email_confirmed_at
-    })
-  }
-  
-  useEffect(() => {
-    getTokenData()
-  }, [])
-
-
-  console.log(authState);
   return (
       <div className="home">
-        <nav>
-          <NavLink to="/"><img src={logo}/></NavLink>
-          <p className='welcome-user'>Welcome back, {userDataObj != undefined && userDataObj.username}</p>
-          <div><img src={mountain}/><p>503 KM</p></div>
-          <NavLink onClick={() => {if (isAccountMenuOpen) {setIsAccountMenuOpen(false);} else {setIsAccountMenuOpen(true);}}}><img src={profile}/></NavLink>
-        </nav>
-        <div className={`account-menu ${isAccountMenuOpen ? 'open' : ''}`}>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
+        <HomeNavbarAuth />
         <div className='content'>
           <div className='grid'>
             <Link to='/explore' className='card'>
