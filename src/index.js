215601d5eac8b0e707cdf1b0bea8dc564b5df3fb
steven-xuk <xuk@scotscollege.school.nz>
2025-05-04 21:31:31 +1200

    idk what those things do but i think service worker pwa thing... also i fixed navbar bug

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +13,4 @@ root.render(
   </Provider>
 );
 
-
+serviceWorkerRegistration.register();
