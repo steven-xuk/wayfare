2610970dde9664453433a7095e280343815bd366
steven-xuk <xuk@scotscollege.school.nz>
2025-05-05 20:30:58 +1200

    did some pwa stuff and download stuff and styling changes idk if itll work but lets try ig

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,12 +6,22 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import back from '../imgs/back.png';
+import logo from '../imgs/logo.png';
 
 
 export default function Login() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const authState = useSelector((state) => state.auth);
+    const [isPWA, setIsPWA] = useState(true);
+
+    useEffect(() => {
+        const isStandalone =
+        window.matchMedia('(display-mode: standalone)').matches ||
+        window.navigator.standalone === true;
+
+        setIsPWA(isStandalone);
+    }, []);
 
     const handleLogin = () => {
         dispatch(login());
@@ -59,9 +69,10 @@ export default function Login() {
     }
 
     return (
-        <div className="login">
+        <div className={`login ${isPWA ? 'pwa' : ''}`}>
+            {isPWA && <div className='title'><img src={logo}/><h1 className='name'>Wayfare</h1></div>}
             <form onSubmit={e => loginToSupabase(e)}>
-                <Link to='/' className="home-link"><img src={back}/><p>Back</p></Link>
+                {!isPWA && <Link to='/' className="home-link"><img src={back}/><p>Back</p></Link>}
                 <h1>Login:</h1>
                 <input placeholder='email' name='email' value={formData.email} onChange={e => handleChange(e)} required/>
                 <input placeholder='password' name='password' type='password' value={formData.password} onChange={e => handleChange(e)} required/>
