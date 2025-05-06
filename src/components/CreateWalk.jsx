73fabeeb7d8e069646292fc08042da6253887fd7
Ice_creme <141097782+ice-creme-studios@users.noreply.github.com>
2025-05-06 13:41:06 +1200

    finished the upload trail functionality

--- src/components/CreateWalk.jsx
+++ src/components/CreateWalk.jsx
@@ -5,6 +5,38 @@ import { supabase } from '../SupabaseClient';
 
 function CreateWalk() {
 
+    const [userDataObj, setUserDataObj] = useState()
+
+    async function getTokenData() {   
+        const { data, error } = await supabase.auth.getSession();
+        if (error) {
+          console.error('Error getting session:', error);
+          return null;
+        }
+    
+        const { data: userData, error: userError } = await supabase
+          .from('Users')      
+          .select('*')
+          .eq('UID', data.session?.user.user_metadata.display_name)
+          .single();
+      
+        if (userError) {
+          console.error('Error fetching row:', userError);
+        }
+        if (userData){
+          // console.log('Fetched row:', userData);
+        }
+        
+        setUserDataObj({
+          ...userData,
+          date_joined: userData.created_at,
+          email: data.session?.user.email,
+          isAuthenticated: data.session?.user.email_confirmed_at
+        })
+      }
+
+
+
     const [walk, setWalk] = useState({
         steps: [],
         MetaData: {
@@ -25,6 +57,7 @@ function CreateWalk() {
             ...prev,
             walkID: ID
         }))
+        getTokenData()
     }, [])
 
 
@@ -38,10 +71,16 @@ function CreateWalk() {
         help: '',
     })
 
+    const [finishingForm, setFinishingForm] = useState({
+        kilometers: '',
+        description: '',
+        difficulty: ''
+    })
+
     const [stepImg, setStepIMG] = useState(null)
 
 
-    async function addStep(e, finish){
+    async function addStep(e){
         e.preventDefault()
         const fileExt = stepImg.name.split('.').pop()
         const fileName = `${Date.now()}.${fileExt}-${ID}`
@@ -69,18 +108,13 @@ function CreateWalk() {
             ...prev,
             steps: [...prev.steps, stepFormData]
         }))
-
-        if(finish){
-            console.log('publishing walk...')
-            publishWalk()
-        }
     }
 
     function publishWalk(){
         console.log(walk)
     }
 
-    function handleChange(e){
+    function handleChangeSteps(e){
         setStepFormData(prev => ({
             ...prev,
             [e.target.name]: e.target.value
@@ -91,6 +125,42 @@ function CreateWalk() {
         setStepIMG(e.target.files[0])
     }
 
+    function handleChangesFinish(e){
+        setFinishingForm(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    async function setFinishData(e){
+        e.preventDefault()
+        console.log(walk)
+
+        const walkDataToUpload = {
+            MetaData: {
+                ...finishingForm,
+                likes: 0,
+                comments: [],
+            },
+            steps: walk.steps,
+            walkID: ID,
+            userData: userDataObj,
+        }
+
+        const { data, error } = await supabase
+        .from('trails')
+        .insert(walkDataToUpload)
+    
+        if (data){
+            console.log(data)
+        }
+        if (error){
+            console.log(error)
+        }
+    }
+
+
+
     return (
         <div>
             <p>create a walk!</p>
@@ -99,16 +169,30 @@ function CreateWalk() {
                     <h2>lets get started by making your first step!</h2>
 
                     <form onSubmit={e => addStep(e)}>
-                        <input name='title' type="text" value={stepFormData.title} placeholder='add a title' required onChange={e => handleChange(e)}/>
-                        <textarea rows="10" cols="50" name='description' type='tel' value={stepFormData.description} placeholder='add a description' required onChange={e => handleChange(e)}/>
-                        <input name='tip' type="text" value={stepFormData.tip} placeholder='add a tip' required onChange={e => handleChange(e)}/>
+                        <input name='title' type="text" value={stepFormData.title} placeholder='add a title' required onChange={e => handleChangeSteps(e)}/>
+                        <textarea rows="10" cols="50" name='description' type='tel' value={stepFormData.description} placeholder='add a description' required onChange={e => handleChangeSteps(e)}/>
+                        <input name='tip' type="text" value={stepFormData.tip} placeholder='add a tip' required onChange={e => handleChangeSteps(e)}/>
                         <input name='img' type="file" placeholder='add an image' required onChange={e => handleImgUpload(e)} accept="image/*"/>
-                        <input name='pin' type='text'  value={stepFormData.pin} placeholder='add a link to google maps' required onChange={e => handleChange(e)}/>
-                        <input name='help' type="text" value={stepFormData.help} placeholder="add a some 'help' text" onChange={e => handleChange(e)}/>
+                        <input name='pin' type='text'  value={stepFormData.pin} placeholder='add a link to google maps' required onChange={e => handleChangeSteps(e)}/>
+                        <input name='help' type="text" value={stepFormData.help} placeholder="add a some 'help' text" onChange={e => handleChangeSteps(e)}/>
                         <button type='submit'>create step</button>
-                        <button type='submit'>create step and finish</button>
                     </form>
+                    <button onClick={() => setCurrentStep('finishing')}>finish</button>
                 
+                </div>}
+
+            {currentStep == 'finishing' && <div>
+                    <p>add finsihing touches</p>                    
+                
+                    <form onSubmit={setFinishData}>
+                        <input placeholder='kilometers (optional)' name='kilometers' type="text" value={finishingForm.kilometers} onChange={e => handleChangesFinish(e)}/>
+                        <input placeholder='description about the trail' name='description' type="text" value={finishingForm.description} onChange={e => handleChangesFinish(e)}/>
+                        <label htmlFor="slider">
+                            Difficulty: {finishingForm.difficulty}
+                        </label>
+                        <input id='slider' placeholder='difficulty' name='difficulty' type='range' min={1} max={10} value={finishingForm.difficulty} onChange={e => handleChangesFinish(e)}/>
+                        <button type='submit'>Create your walk!</button>
+                    </form>
                 </div>}
         </div>
     );
